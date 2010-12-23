73824cf5c284032dd4e5f6de316bd7f74659fe76
Luca Guidi <guidi.luca@gmail.com>
2010-12-23 16:14:11 +0100

    Added Devise views

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Mine::Application.routes.draw do
   devise_for :users
 
-  root :to => "welcome#index"
+  root :to => "home#index"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
