14b7f90899192ebb1addeb64ff92c4dd8d299e72
Luca Guidi <guidi.luca@gmail.com>
2010-12-29 08:37:29 +0100

    Transform tld before validation

--- app/models/subdomain.rb
+++ app/models/subdomain.rb
@@ -1,8 +1,19 @@
 class Subdomain < ActiveRecord::Base
+  attr_accessible :tld, :name
+
   has_many :memberships, :dependent => :destroy
   has_many :users, :through => :memberships
 
   validates_uniqueness_of :tld, :case_sensitive => false
+  validates_length_of     :tld, :name, :maximum => 64
   validates_presence_of   :tld, :name
+
   validates :tld, :ascii => true
+
+  before_validation :transform_tld
+
+  protected
+    def transform_tld
+      self.tld = tld.to_slug.normalize!(:to_ascii => true) unless tld.blank?
+    end
 end
--- spec/models/subdomain_spec.rb
+++ spec/models/subdomain_spec.rb
@@ -13,11 +13,18 @@ describe Subdomain do
   specify { should validate_presence_of(:tld) }
   specify { should validate_presence_of(:name) }
 
+  specify { should ensure_length_of(:name).is_at_most(64) }
+  specify { should ensure_length_of(:tld).is_at_most(64) }
+
+  it "should have accessible attributes" do
+    @subdomain.accessible_attributes.should == [ :tld, :name ]
+  end
+
   describe "tld validation" do
     let(:subdomain) { Factory.build :subdomain, :tld => tld}
 
     context "given an ASCII string" do
-      let(:tld) { 'sushistar' }
+      let(:tld) { "sushistar" }
 
       it "should be valid" do
         subdomain.should be_valid
