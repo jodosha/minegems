608285db9cdbc26976a28bcab62906618fb307fa
Luca Guidi <guidi.luca@gmail.com>
2011-03-03 12:26:26 +0100

    Use proper Redis configuration for production env

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -4,6 +4,14 @@
 # else
 #   Redis.new :thread_safe => true
 # end
-$redis = Redis.connect :thread_safe => true, :url => ENV['REDISTOGO_URL']
+redis_options = { :thread_safe => true }
+
+if Rails.env.production?
+  uri = URI.parse(ENV["REDISTOGO_URL"])
+  redis_options.merge!({ :host => uri.host, :port => uri.port, :password => uri.password })
+end
+
+$redis = Redis.new(redis_options)
+
 Minegems::Rack::SubdomainRouter.ensure_consistent_lookup!
 Minegems::Rack::SubdomainRouter.ensure_consistent_access!
